c8753a6cec93d10948ffe895530e4a4aae3f47e2
James Kelly <47066363+Saffiuddin@users.noreply.github.com>
2019-02-13 22:10:40 +0530

    Add files via upload

---
+++ word_guess_name.py
@@ -0,0 +1,77 @@
+import random
+Name = input('Enter your name: ')
+def randstring():
+    #four_words variable contain 4 letter random words
+    four_words = "aahs aals abas abba abbe abed abet able ably abos abri abut abye abys aced aces ache achy acid acme acne acre acta acts acyl adds adit ados adze aeon aero aery afar agar agas aged agee ager ages agha agin agio agly agma agog agon ague ahed ahem ahis ahoy aide aids ails aims ains airn airs airt airy aits ajar ajee akee akin alae alan alar alas alba albs alec alee alef ales alfa alga alif alit alky alls ally alma alme alms aloe alow alps also alto alts alum amah amas ambo amen amia amid amie amin amir amis ammo amok amps amus amyl anal anas ands anes anew anga anil anis ankh anna anoa anon ansa anta ante anti ants anus aped aper apes apex apod apos apps apse aqua arak arbs arch arco arcs area ares arfs aria arid aril arks arms army arse arts arty arum arvo aryl asci asea ashy asks asps atap ates atma atom atop auks auld aunt aura auto aver aves avid avos avow away awed awee awes awls awns awny awol awry axal axed axel axes axil axis axle axon ayah ayes ayin azan azon baal baas baba babe babu baby bach back bade bads baff bags baht bail bait bake bald bale balk ball balm bals bams band bane bang bani bank bans baps barb bard bare barf bark barm barn bars base bash bask bass bast bate bath bats batt baud bawd bawl bays bead beak beam bean bear beat beau beck beds bedu beef been beep beer bees beet begs bell bels belt bema bend bene bens bent berg berk berm best beta beth bets bevy beys bhut bias bibb bibs bice bide bidi bids bier biff bigs bike bile bilk bill bima bind bine bins bint biog bios bird birk birl biro birr bise bisk bite bits bitt bize blab blae blah blam blat blaw bleb bled blet blew blin blip blob bloc blog blot blow blub blue blur boar boas boat bobs bock bode bods body boff bogs bogy boho boil bola bold bole boll bolo bolt bomb bond bone bong bonk bony boob book boom boon boor boos boot bops bora bore bork born bort bosh bosk boss bota both bots bott bout bowl bows boxy boyo boys bozo brad brae brag bran bras brat braw bray bred bree bren brew brie brig brim brin brio bris brit broo bros brow brrr brut brux bubo bubs bubu buck buds buff bugs buhl buhr bulb bulk bull bumf bump bums buna bund bung bunk bunn buns bunt buoy bura burb burd burg burl burn burp burr burs bury bush busk buss bust busy bute buts butt buys buzz byes byre byrl byte cabs caca cade cadi cads cafe caff cage cagy caid cain cake caky calf calk call calm calo calx came camo camp cams cane cans cant cape caph capo caps carb card care cark carl carn carp carr cars cart casa case cash cask cast cate cats caul cave cavy caws cays ceca cede cedi cees ceil cell cels celt cent cepe ceps cere cero cess cete chad chai cham chao chap char chat chaw chay chef chew chez chia chic chid chin chip chis chit chon chop chow chub chug chum ciao cigs cine cion cire cist cite city clad clag clam clan clap claw clay clef clew clip clod clog clon clop clot cloy club clue coal coat coax cobb cobs coca cock coco coda code cods coed coff coft cogs coho coif coil coin coir coke coky cola cold cole cols colt coly coma comb come comp cone coni conk conn cons cony coof cook cool coon coop coos coot cope cops copy cord core corf cork corm corn cors cory cosh coss cost cosy cote cots coup cove cowl cows cowy coxa coys cozy crab crag cram crap craw cred crew crib cris crit croc crop crow crud crus crux cube cubs cuds cued cues cuff cuif cuke cull culm cult cunt cups curb curd cure curf curl curn curr curs curt cusk cusp cuss cute cuts cwms cyan cyma cyme cyst czar dabs dace dada dado dads daff daft dago dags dahl dahs dais daks dale dals dame damn damp dams dang dank dans daps darb dare dark darn dart dash data date dato daub daut davy dawk dawn daws dawt days daze dead deaf deal dean dear debs debt deck deco deed deem deep deer dees deet defi deft defy deil deke dele delf deli dell dels delt deme demo demy dene deni dens dent deny dere derm desk deva devs dews dewy dexy deys dhak dhal dhow dial dibs dice dick dido didy died diel dies diet diff difs digs dike dill dime dims dine ding dink dino dins dint diol dips dipt dire dirk dirl dirt disc dish disk diss dita dite dits ditz diva dive djin doat doby dock docs dodo doer does doff doge dogs dogy doit dojo dole doll dols dolt dome doms dona done dong dons doom door dopa dope dopy dore dork dorm dorp dorr dors dory dose doss dost dote doth dots doty doum dour doux dove down dows doxy doze dozy drab drag dram drat draw dray dree dreg drek drew drib drip drop drub drug drum drys duad dual dubs duce duci duck duine fink fino fins fire  nonert very vest veto vets vext vial vibe vice vide vids vied vier vies view viga vigs vile vill vims vina vine vino viny viol virl visa vise vita viva vive voes void vole volt vote vows vrow vugg vugh vugs wabs wack wade wadi wads wady waes waff waft wage wags waif wail wain wair wait wake wale walk wall waly wame wand wane wank wans want wany waps ward ware wark warm warn warp wars wart wary wash wasp wast wats watt wauk waul waur wave wavy wawl waws waxy ways weak weal wean wear webs weds weed week weel ween weep weer wees weet weft weir weka weld well welt wend wens went wept were wert west wets wham whap what whee when whet whew whey whid whig whim whin whip whir whit whiz whoa whom whop whup whys wich wick wide wife wigs wild wile will wilt wily wimp wind wine wing wink wino wins winy wipe wire wiry wise wish wisp wiss wist wite with wits wive woad woes wogs woke woks wold wolf womb wonk wons wont wood woof wool woos wops word wore work worm worn wort wost wots wove wows wrap wren writ wuss wych wyes wyle wynd wynn wyns wyte xyst yack yaff yagi yags yaks yald yams yang yank yaps yard yare yarn yaud yaup yawl yawn yawp yaws yays yeah yean year yeas yech yegg yeld yelk yell yelp yens yeps yerk yeti yett yeuk yews yids yill yins yipe yips yird yirr ylem yobs yock yodh yods yoga yogh yogi yoke yoks yolk yond yoni yore your yous yowe yowl yows yuan yuca yuch yuck yuga yuks yule yups yurt yutz ywis zags zany zaps zarf zeal zebu zeds zees zein zeks zeps zerk zero zest zeta zigs zill zinc zine zing zins zips ziti zits zoea zoic zona zone zonk zoom zoon zoos zori zouk zyme "
+    valid_letters = four_words.split()#to split the sentence into four words
+    return ''.join((random.choice(valid_letters)))# the command is to select the random words from the valid letters list
+s_word=randstring()
+g_word=''#guess word
+g_count=1# this is guessing count to limit the guess
+out_of_guess=False
+while g_word !=s_word and not(out_of_guess)   :#This while is for give the chance unitl the chance are over
+    if g_count <16:#this if check the chance over or not if over come out
+
+        g_word=input('Enter the guess of the secret word:   ')[:4]
+        if g_word == s_word:
+           no=str(g_count)#to convert the g_count to str becoz v can't concat str and int
+           print( 'GREAT!!! '+Name+' guessed word is correct, ' +Name+ ' win by ' + no +  '  guess')
+           out_of_guess=True#After find the value change the boolean to stop the looping
+        else:
+           for x in range (0,4):
+             if s_word[x] == g_word[x]:#to compare the each char of g_word and s_word
+               if x == 0:
+                   k=g_word[x]
+               else:
+                   k=k+g_word[x]
+
+             else:
+                 if x ==  0:
+                     k = '_'
+                 else :
+                     k = k + '_'
+           print(k)
+
+
+
+           for k in range(0, 4):#this loop is for comparing the element in all possible ways
+               boolean = True#this boolean value is for avoid the comparing the word that already found
+               for j in range(0, 4):
+                   if s_word[k] == g_word[j]:#it filter the word that not matched
+                       if j != k and boolean == True:#this filter the already finded exact  and approx. pos. of the letter(07)
+                           print('Letter ' + g_word[j] + ' is  present in wrong position')
+                           boolean = False#if we find the value this change the value to false to false the if(7) condition
+
+           rem=15-g_count#for showing remaing chance
+           rem=str(rem)
+           print('Remaining chance ' +rem )
+           g_count += 1
+           count=str(g_count)
+           print('Guessing count is '+count)
+    else:
+
+      print('Out of chance, U lose!!!')
+      print('The secret word is  '+s_word)
+      break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
